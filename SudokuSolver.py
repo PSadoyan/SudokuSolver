7ca7a01ddf3133de1f845edb08dd97ede69d0760
PSadoyan <pavelsadoyan@gmail.com>
2023-10-07 17:02:24 -0500

    Debugged solve function, saving before refactoring

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -5,81 +5,79 @@ class SudokuGrid:
     y = 0
     n = 1
     Grid = np.array(
-        [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,7,9]])
+        [[0,0,0,0,9,0,0,2,0],
+        [4,0,2,5,0,0,0,6,0],
+        [0,5,3,0,7,0,0,4,0],
+        [0,7,8,0,0,1,0,0,3],
+        [9,0,0,0,5,0,0,0,0],
+        [0,4,0,6,0,0,0,0,0],
+        [0,0,0,0,0,7,0,0,2],
+        [5,0,0,0,4,0,7,0,0],
+        [0,0,0,0,0,0,1,0,6]])
     
     def _init_(self):
-        self.Grid = np.array(
-        [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,7,9]])
-
-    def isZero(self,x,y):
-        if self.Grid[x][y] == 0:
-            return True
-        
-    def insert(self,x,y,n):
-        if self.isZero(x,y):
+        self.Grid = np.array( #valid Sudoku game
+        [[0,0,0,0,9,0,0,2,0],
+        [4,0,2,5,0,0,0,6,0],
+        [0,5,3,0,7,0,0,4,0],
+        [0,7,8,0,0,1,0,0,3],
+        [9,0,0,0,5,0,0,0,0],
+        [0,4,0,6,0,0,0,0,0],
+        [0,0,0,0,0,7,0,0,2],
+        [5,0,0,0,4,0,7,0,0],
+        [0,0,0,0,0,0,1,0,6]])
+ 
+    def insert(self,x,y,n): #O(1)
             self.Grid[x][y] = n
 
-    def validate(self,x,y,n):
+    def validate(self,x,y,n): #O(1) 
         for i in range (x-x%3,(x-x%3)+3):
             for j in range (y-y%3,(y-y%3)+3):
-                 if n == self.Grid[i][j]:
+                 if self.Grid[i][j] == n:
                     return False
                  
         for i in range (9):
-            if n == self.Grid[i][y]:
+            if self.Grid[i][y] == n:
                 return False
             
         for i in range (9):
-            if n == self.Grid[x][i]:
+            if self.Grid[x][i] == n:
                 return False
             
         return True
     
-    def matrixValidate(self):
+    def matrixValidate(self): #O(1): larger constant, but still a constant
         for i in range (9):
             for j in range (9):
-                self.validate(i,j,self.Grid[i][j]) and self.Grid[i][j] != 0
+                self.validate(i,j,self.Grid[i][j]) and self.Grid[i][j] > 0 and self.Grid[i][j] < 10
     
-    def solve(self,x,y,n):
-        print(str(x) + " x")
-        print(str(y) + " y")
-        if (y==9) or ((x==8) and (y==8) and (n==10) and (self.Grid[x-1][y-1] == 0)):
+    def solve(self,x,y,n): #O(1)
+        print(str(x) + " x " + str(n))
+        print(str(y) + " y " + str(n))
+        if (y==9):
             if self.matrixValidate():
-                return self.Grid
+                return True
             
         if (x==9):
             return self.solve(0,y+1,1)
         
         if ((x==8) and (y==8)):
-            self.insert(x,y,n)
-            return self.Grid
+            if self.validate(x,y,n):
+                self.insert(x,y,n)
+                return self.Grid
+            return self.solve(x,y,n+1)
         
         if self.Grid[x][y] > 0:
             return self.solve(x+1,y,1)
         
         if self.Grid[x][y] == 0:
-            if n == 10:
-                return self.solve(x+1,y,1)
+            #if n == 10:
+            #    return self.solve(x+1,y,1)
             if self.validate(x,y,n):
                 self.insert(x,y,n)
                 return self.solve(x+1,y,1)
             return self.solve(x,y,n+1)
+        return False
 
 game1 = SudokuGrid()
 game1.solve(0,0,1)
